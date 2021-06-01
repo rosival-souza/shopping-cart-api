25e31a5417f3f23ea456794834531eb0fa7afad3
Rosival de Souza Maria <psrobrosival@fineasytech.com>
2021-05-31 23:01:49 -0300

    fourth-commit

--- src/api/resolvers.js
+++ src/api/resolvers.js
@@ -13,12 +13,29 @@ module.exports ={
     Mutation:{
         async createProduct (_, { input }){
             const result = await db('products').insert({
-                /* test */ 
-                title: input.title
+                photo: input.photo,
+                title: input.title,
+                price: input.price,
+                reference: input.reference,
+                amount: input.amount,
+                available: input.available,
             })
 
             const id = result[0]
             return await db('products').where({ id }).first()
+        },
+        async createSales (_, { input }){
+            const result = await db('sales').insert({
+                title: input.title,
+                price: input.price,
+                reference: input.reference,
+                amount: input.amount,
+                available: input.available,
+                id_product: input.id_product
+            })
+
+            const id = result[0]
+            return await db('sales').where({ id }).first()
         }
     }
 }
